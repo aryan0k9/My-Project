53dd80d2a93e69d87214bac6850411404ce1e5c4
aryan0k9 <84584531+aryan0k9@users.noreply.github.com>
2023-02-08 15:18:40 +0530

    Add files via upload

---
+++ aevytvClone/app.js
@@ -0,0 +1,10 @@
+let navbar = document.querySelector('.nav')
+
+function toggelnav(){
+    if(navbar.style.display === 'block'){
+        navbar.style.display = 'none'
+    }
+    else{
+        navbar.style.display = 'block';
+    }   
+}
